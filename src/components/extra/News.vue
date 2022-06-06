17e2c62bb6c431b3bd8ab5211045ff0383c7baf1
Sacramentix <benji.u1225u@gmail.com>
2022-06-06 20:09:29 +0200

    add news

---
+++ src/components/extra/News.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3>{{ t('news') }}</h3>
+    <p>
+        {{ t('content') }}
+    </p>
+</template>
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n({ useScope: 'local' });
+</script>
+<i18n lang="yaml">
+  en:
+    news: "News"
+    content: "Dark mode coming soon!"
+  fr:
+    news: "News"
+    content: "Le thème sombre arrive bientôt!"
+</i18n>
+
