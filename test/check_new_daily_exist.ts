0c13c22394ec91125b03f1a08a5d3e25ed9b8432
Sacramentix <benji.u1225u@gmail.com>
2022-07-03 19:20:20 +0200

    try to fix CI with env

--- test/check_new_daily_exist.ts
+++ test/check_new_daily_exist.ts
@@ -2,7 +2,7 @@ import { assert } from "https://deno.land/std@0.146.0/testing/asserts.ts";
 import { stringify } from "https://deno.land/std@0.140.0/node/querystring.ts";
 
 const gitDiff = Deno.run({
-    cmd: ["git", "diff", "--name-only", "HEAD^", " HEAD"],
+    cmd: ["git", "diff", "--name-only", "origin/main", `origin/${Deno.env.get("GITHUB_HEAD_REF")}`],
     stdout: 'piped',
     stderr: 'piped',
     stdin: 'null'
