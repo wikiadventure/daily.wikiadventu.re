fd31b5c928ca12bc4e758112b565ae6ddc3a60b6
Sacramentix <benji.u1225u@gmail.com>
2025-07-02 19:21:46 +0200

    fix default lang and improve / redirect

--- src/composables/gameForm.ts
+++ src/composables/gameForm.ts
@@ -20,14 +20,11 @@ export interface GameFormState {
     setReverse: (b: boolean) => void;
 }
 
-export function defaultWikiLang(AstroRequest?: { url: string }): DailyWikiLang {
+export function defaultWikiLang(): DailyWikiLang {
     if (typeof document !== "undefined") {
         const currentUrl = document.location.pathname;
         const parsedLocal = currentUrl.split("/").at(1);
-        return (dailyAvailableIn.includes(parsedLocal as DailyWikiLang) ? parsedLocal : "en") as DailyWikiLang;
-    } else if (AstroRequest) {
-        const currentUrl = new URL(AstroRequest.url).pathname;
-        const parsedLocal = currentUrl.split("/").at(1);
+        console.log({parsedLocal});
         return (dailyAvailableIn.includes(parsedLocal as DailyWikiLang) ? parsedLocal : "en") as DailyWikiLang;
     }
     return "en";
@@ -39,7 +36,7 @@ export const useGameFormStore = create(persist(
         setDailyDate: (date: Date) => 
             set((state:GameFormState) => ({ ...state, dailyDate: date }) satisfies GameFormState),
 
-        wikiLang: null as unknown as DailyWikiLang,
+        wikiLang: defaultWikiLang() as DailyWikiLang,
         setWikiLang: (lang: DailyWikiLang) => {
             lang && 
             set((state:GameFormState) => ({ ...state, wikiLang: lang }) satisfies GameFormState);
